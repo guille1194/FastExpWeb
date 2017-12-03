e8dc62d55d0fa9f62e505976bb385bbc50fdfd3a
guille1194 <guille1194@gmail.com>
2017-12-02 21:06:20 -0800

    creacion sidenav v1

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -12,7 +12,7 @@ import {AppRoutingModule} from '../app-routing.module';
         AppRoutingModule
     ],
     exports: [
-        AppRoutingModule
+        AppRoutingModule,
     ],
     declarations: [],
     providers: [
