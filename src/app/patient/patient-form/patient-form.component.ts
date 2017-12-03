059757d3de1c0672da495b6f8fe2ff8a5b66db13
Alan Flores <borreh.2814@gmail.com>
2017-12-03 00:45:20 -0800

    Update form to add patients

--- src/app/patient/patient-form/patient-form.component.ts
+++ src/app/patient/patient-form/patient-form.component.ts
@@ -5,6 +5,9 @@ import {Patient} from '../shared/patient.model';
 import {PatientPersonal} from '../shared/patient.personal.model';
 import {PatientMedical} from '../shared/patient.medical.model';
 import {PatientService} from '../shared/patient.service';
+import {Address} from '../../shared/models/address.model';
+import {EmergencyContact} from '../../shared/models/emergency-contact.model';
+import {Disease} from '../../shared/models/disease.model';
 
 @Component({
   selector: 'patient-create',
@@ -13,9 +16,6 @@ import {PatientService} from '../shared/patient.service';
 
 export class PatientFormComponent {
   patientFormGroup: FormGroup;
-  addressFormGroup: FormGroup;
-  contactFormGroup: FormGroup;
-  diseaseFormGroup: FormGroup;
   formBuilder: FormBuilder;
   patient: Patient;
 
@@ -23,9 +23,27 @@ export class PatientFormComponent {
               private router: Router,
               private patientService: PatientService) {
     this.formBuilder = formBuilder;
+
     this.patient = new Patient();
+
     this.patient.personal = new PatientPersonal();
+    this.patient.personal.addresses = [];
+    this.patient.personal.addresses.push(new Address());
+    this.patient.personal.phoneNumbers = [];
+    this.patient.personal.emergencyContacts = [];
+    this.patient.personal.emergencyContacts.push(new EmergencyContact());
+    this.patient.personal.emergencyContacts[0].phoneNumbers = [];
+
     this.patient.medical = new PatientMedical();
+    this.patient.medical.chronicDiseases = [];
+    this.patient.medical.chronicDiseases.push(new Disease());
+    this.patient.medical.chronicDiseases[0].treatments = [];
+    this.patient.medical.allergies = [];
+    this.patient.medical.allergies.push(new Disease());
+    this.patient.medical.allergies[0].treatments = [];
+    this.patient.medical.disabilities = [];
+    this.patient.medical.disabilities.push(new Disease());
+    this.patient.medical.disabilities[0].treatments = [];
   }
 
   ngOnInit() {
@@ -37,27 +55,25 @@ export class PatientFormComponent {
       name: ['', Validators.required],
       lastName: ['', Validators.required],
       birthDate: ['', Validators.required],
+      street: [''],
+      number: [''],
+      locality: [''],
+      county: [''],
+      state: [''],
+      country: [''],
+      zipCode: [''],
+      phoneNumber: [''],
+      contactName: ['', Validators.required],
+      contactLastName: ['', Validators.required],
+      contactPhoneNumber: ['', Validators.required],
       picture: [''],
-      bloodType: ['', Validators.required]
-    });
-
-    this.addressFormGroup = formBuilder.group({
-      street: ['', Validators.required],
-      number: ['', Validators.required],
-      locality: ['', Validators.required],
-      county: ['', Validators.required],
-      state: ['', Validators.required],
-      country: ['', Validators.required],
-      zipCode: ['', Validators.required]
-    });
-
-    this.contactFormGroup = formBuilder.group({
-      name: ['', Validators.required],
-      lastName: ['', Validators.required]
-    });
-
-    this.diseaseFormGroup = formBuilder.group({
-      name: ['', Validators.required]
+      bloodType: ['', Validators.required],
+      chronicDiseaseName: [''],
+      chronicDiseaseTreatment: [''],
+      allergyName: [''],
+      allergyTreatment: [''],
+      disabilityName: [''],
+      disabilityTreatment: ['']
     });
   }
 
